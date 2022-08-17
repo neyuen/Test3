13e730eb2533f69e90c2c40670e4c60140e88bf1
Thu Nguyen <thu126129@gmail.com>
2022-08-17 14:54:29 -0500

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Thu Nguyen on 8/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
